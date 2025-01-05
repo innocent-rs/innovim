67366baaf9de01a9ad4619d32a3f9e6fa5827900
kyuno <kyuno@protonmail.com>
2025-01-05 13:58:13 +0100

    add lint & ruff

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -179,7 +179,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -219,6 +219,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
