e1c803d04e9ed59866c77670c4a196d84c4c5df9
kyuno <kyuno@protonmail.com>
2025-01-05 13:39:03 +0100

    moved to auto import all plugins (so I don't have to check manually which plugin is on.) + stylua.toml

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,6 @@
-require("lazy").setup({
-  "tpope/vim-sleuth",
-  require("plugins/gitsigns"),
-  require("plugins/which-key"),
-  require("plugins/telescope"),
-  require("plugins/cmp"),
-  require("plugins/lsp"),
-  require("plugins/conform"),
-  require("plugins/treesitter"),
-  require("plugins/neo-tree"),
-  require("plugins/mini"),
-  require("plugins/autopairs"),
-  require("plugins/indent_line"),
-  require("plugins/todo-comments"),
-  require("plugins/theme"),
+require('lazy').setup({
+  'tpope/vim-sleuth',
+  { import = 'core.plugins' },
 }, {
   ui = {
     icons = {},
