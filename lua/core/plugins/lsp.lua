5f481b33e21556500c2b39ab5f451ca9d102fdbc
kyuno <kyuno@protonmail.com>
2025-01-11 22:33:30 +0100

    tmux: fix issues with keybindings

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -190,7 +190,6 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
-        --
         cssls = {},
         html = {},
         nil_ls = {
